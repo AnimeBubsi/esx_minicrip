357babd3c17364a0ada08c4514701625cd8511b3
Bubsi <95187459+AnimeBubsi@users.noreply.github.com>
2022-08-17 15:31:15 +0300

    Update client.lua

--- esx_minicrip/client.lua
+++ esx_minicrip/client.lua
@@ -3,7 +3,7 @@ ESX = nil
 Citizen.CreateThread(function()
 	while ESX == nil do
 		TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-		Citizen.Wait(0)
+		Citizen.Wait(500)
 	end
 end)
 
